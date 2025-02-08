1b6be32fcf104951c407eb22ae0180a46b584209
Mustafa Buğra Avcı <mbugraavci38@gmail.com>
2025-02-08 22:17:50 +0000

    check

--- src/config/nasa.ts
+++ src/config/nasa.ts
@@ -4,4 +4,4 @@ if (!NASA_API_KEY) {
   console.warn('NASA API key is not set. Please add it to your .env file.');
 }
 
-export const NASA_API_BASE_URL = 'https://api.nasa.gov';
+export const NASA_API_BASE_URL = 'https://api.nasa.gov/planetary/v2/eclipses';
